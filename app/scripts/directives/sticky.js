000f36ea2b80097354346c7a8e5b86a6ac0cd964
Gary Paige <gary.paige@savvis.com>
2015-03-26 18:39:58 -0500

    updated for horizontal scrolling

--- app/scripts/directives/sticky.js
+++ app/scripts/directives/sticky.js
@@ -12,10 +12,20 @@ angular.module('iLayers')
       restrict: 'A',
       link: function postLink(scope, element, attrs) {
         var main = element.parent('main'),
-            offset = (attrs.offset) ? attrs.offset : 0;
+            offset = (attrs.offset) ? attrs.offset : 0,
+            locked = element.find('.lock-horizon');
+
+        console.log("locked ", locked);
+
 
         main.bind('scroll', function() {
-          var top = main.scrollTop();
+          var top = main.scrollTop(),
+              left = main.scrollLeft();
+
+          if (locked) {
+            locked.css('left',  '-' + $(this).scrollLeft() + 'px');
+          }
+
           if (main.scrollTop() >= offset) {
             element.addClass('sticky');
           } else {
