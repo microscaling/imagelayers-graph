565434a4f3815c031e681c0b963c5cf35902aab5
Gary Paige <gary.paige@savvis.com>
2015-04-28 09:51:43 -0500

    retry api calls within registryService

--- app/scripts/services/registryservice.js
+++ app/scripts/services/registryservice.js
@@ -1,40 +1,72 @@
 'use strict';
 
 angular.module('iLayers')
-  .factory('registryService', ['$http', 'errorService', 'ENV',
-    function($http, errorService, ENV) {
-
+  .factory('registryService', ['$http', '$q', 'errorService', 'ENV',
+    function($http, $q, errorService, ENV) {
+      
+      function retryQuery(queryFn, maxRetry) {
+        var callResults = $q.defer(),
+            count = 1;
+        
+        function doCall() {
+          queryFn().then(
+            function(response) {
+              callResults.resolve(response);
+            },
+            function(response) {
+              if (count < maxRetry) {
+                count++;
+                // Backoff timer 0 - 750ms 
+                setTimeout(doCall, (750 * Math.random()));
+              } else {
+                response.retryLimit = true;
+                callResults.reject(response);
+              }
+          }); 
+        };
+        
+        doCall();
+        return callResults.promise;
+      };
+      
       return {
           inspect: function (list) {
-            var promise = $http.post(ENV.apiEndpoint + '/registry/analyze', { 'repos': list }).then(function(response) {
-              var images = response.data,
-                  list = [],
-                  missing = [];
-              
-              angular.forEach(images, function(image) {
-                if (image.layers !== null) { 
-                  image.layers = image.layers.reverse();
-                  list.push(image);
-                } else {
-                  missing.push(image.repo.name + ':' + image.repo.tag);
-                }
-              });
-              
-              if (missing.length >0) {
-                errorService.notification('The following images could not be found: ' + missing.join(' ')); 
-              }
-              
-              response.data = list;
-              return response;
-            });
+            function doInspection() {
+              return $http.post(ENV.apiEndpoint + '/registry/analyze', { 'repos': list })
+                .then(function(response) {
+                  var images = response.data,
+                      list = [],
+                      missing = [];
 
-            return promise;
+                  angular.forEach(images, function(image) {
+                    if (image.layers !== null) { 
+                      image.layers = image.layers.reverse();
+                      list.push(image);
+                    } else {
+                      missing.push(image.repo.name + ':' + image.repo.tag);
+                    }
+                  });
+
+                  if (missing.length >0) {
+                    errorService.notification('The following images could not be found: ' + missing.join(' ')); 
+                  }
+
+                  response.data = list;
+                  return response;
+                });
+            };
+              
+            return retryQuery(doInspection, 3);
           },
           search: function(name) {
-            return $http.get(ENV.apiEndpoint + '/registry/search?name='+name);
+            return retryQuery(function() {
+              return $http.get(ENV.apiEndpoint + '/registry/search?name='+name);
+            }, 3);
           },
           fetchTags: function(name) {
-            return $http.get(ENV.apiEndpoint + '/registry/images/'+name+'/tags');
+            return retryQuery(function() {
+              return $http.get(ENV.apiEndpoint + '/registry/images/'+name+'/tags');
+            }, 3);
           }
       };
   }]);
--- test/spec/services/registryservice.js
+++ test/spec/services/registryservice.js
@@ -6,13 +6,68 @@ describe('Service: registryService', function () {
   beforeEach(module('iLayers'));
 
   // instantiate service
-  var registryService;
-  beforeEach(inject(function (_registryService_) {
+  var httpBackend, requestHandler, env, registryService, errorService;
+  
+  beforeEach(inject(function ($httpBackend, _ENV_, _registryService_, _errorService_) {
     registryService = _registryService_;
+    errorService = _errorService_;
+    httpBackend = $httpBackend;
+    env = _ENV_;
+    env.apiEndpoint = 'http://localhost';
   }));
-
-  it('should do something', function () {
-    expect(!!registryService).toBe(true);
+  
+  afterEach(function() {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
   });
 
+  describe('inspect', function() {
+    beforeEach(function() {
+      requestHandler = httpBackend.whenPOST('http://localhost/registry/analyze');
+      requestHandler.respond(200, [{ layers: [] }]);
+      spyOn(errorService, 'notification');
+    });
+    
+    it('should return list of images', function() {
+      httpBackend.expectPOST('http://localhost/registry/analyze');
+      registryService.inspect([]);
+      httpBackend.flush();
+    });
+    
+    it('should call errorService.notification when image not found', function() {
+      requestHandler.respond(200, [{ layers: null, repo: { name: 'foo', tag: 'bar' } }]);
+      httpBackend.expectPOST('http://localhost/registry/analyze');
+      registryService.inspect([]);
+      httpBackend.flush();
+      
+      expect(errorService.notification).toHaveBeenCalled();
+    });
+    
+  });
+  
+  describe('search', function() {
+     beforeEach(function() {
+      requestHandler = httpBackend.whenGET('http://localhost/registry/search?name=foo');
+      requestHandler.respond(200, '');
+    });
+    
+    it('should call search url', function() {
+      httpBackend.expectGET('http://localhost/registry/search?name=foo');
+      registryService.search('foo');
+      httpBackend.flush();
+    });
+  });
+  
+  describe('fetchTags', function() {
+    beforeEach(function() {
+      requestHandler = httpBackend.whenGET('http://localhost/registry/images/foo/tags');
+      requestHandler.respond(200, '');
+    });
+    
+    it('should call tags url', function() {
+      httpBackend.expectGET('http://localhost/registry/images/foo/tags');
+      registryService.fetchTags('foo');
+      httpBackend.flush();
+    });
+  });
 });
