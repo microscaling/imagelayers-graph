e4789c62aaff9b081dcdc454f7d0b3c078517856
Gary Paige <gary.paige@savvis.com>
2015-05-04 10:54:27 -0500

    change byte calculation from 1024 to 1000

--- app/scripts/filters/size.js
+++ app/scripts/filters/size.js
@@ -6,8 +6,8 @@ angular.module('iLayers')
        var sizes = ['bytes', 'kb', 'mb', 'gb', 'tb'];
        if (bytes === 1) { return '1 <span>byte</span>'; }
        if (bytes === 0) { return '0 <span>bytes</span>'; }
-       var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-       return Math.round(bytes / Math.pow(1024, i), 2) + ' <span>' + sizes[i] + '</span>';
+       var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)));
+       return Math.round(bytes / Math.pow(1000, i), 2) + ' <span>' + sizes[i] + '</span>';
     };
 
     return function(input) {
