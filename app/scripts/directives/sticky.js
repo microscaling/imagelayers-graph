60dda40130c6ebdcea394cbd27b25a531673fceb
Laura Frank <ljfrank@gmail.com>
2015-07-16 13:20:19 -0500

    Fixing scrolling when graph and inner window height are nearly the same

--- app/scripts/directives/sticky.js
+++ app/scripts/directives/sticky.js
@@ -17,23 +17,7 @@
         var main = element.closest('main'),
             offset = (attrs.offset) ? attrs.offset : 0;
 
-        scope.heightSet = false;
-
-        scope.setHeight = function () {
-          if (scope.heightSet) return;
-
-          var matrixHeight = $('.matrix').height(),
-            windowHeight = window.innerHeight,
-            mainHeight = $('main').height();
-
-          if (matrixHeight - mainHeight < offset) {
-            $('.matrix').css('height', windowHeight);
-          }
-          scope.heightSet = true;
-        };
-
         main.bind('scroll', function() {
-          scope.setHeight();
           if (main.scrollTop() >= offset) {
             element.addClass('sticky');
             $('.matrix').css('padding-top', 160);
