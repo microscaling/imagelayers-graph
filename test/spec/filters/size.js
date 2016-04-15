305f81dc030a47dfd1ec3783dfc52482fa0433f6
Thomas Grainger <tom.grainger@procensus.com>
2016-04-15 15:25:51 +0100

    Replace units for kilobits, millibars, grambits, and tablespoons with SI data units

--- test/spec/filters/size.js
+++ test/spec/filters/size.js
@@ -8,39 +8,39 @@ describe('Size Filter', function() {
     filter = $filter;
   }));
 
-  it('should convert 0 to 0 bytes', function() {
+  it('should convert 0 to 0 Bytes', function() {
     var result = filter('size')(0);
-    expect(result.$$unwrapTrustedValue()).toEqual('0 <span>bytes</span>');
+    expect(result.$$unwrapTrustedValue()).toEqual('0 <span>Bytes</span>');
   });
 
   it('should convert 1 to 1 byte', function() {
     var result = filter('size')(1);
-    expect(result.$$unwrapTrustedValue()).toEqual('1 <span>byte</span>');
+    expect(result.$$unwrapTrustedValue()).toEqual('1 <span>Byte</span>');
   });
 
   it('should convert 10 to 10 bytes', function() {
     var result = filter('size')(10);
-    expect(result.$$unwrapTrustedValue()).toEqual('10 <span>bytes</span>');
+    expect(result.$$unwrapTrustedValue()).toEqual('10 <span>Bytes</span>');
   });
 
-  it('should convert 1024 to 1 kb', function() {
+  it('should convert 1024 to 1 KiB', function() {
     var result = filter('size')(1024);
-    expect(result.$$unwrapTrustedValue()).toEqual('1 <span>kb</span>');
+    expect(result.$$unwrapTrustedValue()).toEqual('1 <span>KiB</span>');
   });
 
-  it('should convert 1,048,576 to 1 mb', function() {
+  it('should convert 1,048,576 to 1 MiB', function() {
     var result = filter('size')(1048576);
-    expect(result.$$unwrapTrustedValue()).toEqual('1 <span>mb</span>');
+    expect(result.$$unwrapTrustedValue()).toEqual('1 <span>MiB</span>');
   });
 
-  it('should convert 1,073,741,824 to 1 gb', function() {
+  it('should convert 1,073,741,824 to 1 GiB', function() {
     var result = filter('size')(1073741824);
-    expect(result.$$unwrapTrustedValue()).toEqual('1 <span>gb</span>');
+    expect(result.$$unwrapTrustedValue()).toEqual('1 <span>GiB</span>');
   });
 
-  it('should convert 1,099,511,627,776 to 1 tb', function() {
+  it('should convert 1,099,511,627,776 to 1 TiB', function() {
     var result = filter('size')(1099511627776);
-    expect(result.$$unwrapTrustedValue()).toEqual('1 <span>tb</span>');
+    expect(result.$$unwrapTrustedValue()).toEqual('1 <span>TiB</span>');
   });
 
 });
