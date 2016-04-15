0d8fa53eaeed4bc45c32a7f9f95edf6cfc6f6404
Thomas Grainger <tom.grainger@procensus.com>
2016-04-15 15:25:51 +0100

    Replace units for kilobits, millibars, grambits, and tablespoons with SI data units

--- app/scripts/filters/size.js
+++ app/scripts/filters/size.js
@@ -4,9 +4,9 @@
   angular.module('iLayers')
     .filter('size', ['$sce', function($sce) {
       var bytesToSize = function(bytes) {
-         var sizes = ['bytes', 'kb', 'mb', 'gb', 'tb'];
-         if (bytes === 1) { return '1 <span>byte</span>'; }
-         if (bytes === 0) { return '0 <span>bytes</span>'; }
+         var sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB'];
+         if (bytes === 1) { return '1 <span>Byte</span>'; }
+         if (bytes === 0) { return '0 <span>Bytes</span>'; }
          var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)));
          return Math.round(bytes / Math.pow(1000, i), 2) + ' <span>' + sizes[i] + '</span>';
       };
